a6538fe8c5ad464b7b1e093e6c7e430c1561d28a
elnaz <fatemeharash7@gmail.com>
2025-06-01 08:02:44 +0330

    feature: زاشدلث زخمخق ساشیخص

--- kit/src/lib/components/theme/shadow.ts
+++ kit/src/lib/components/theme/shadow.ts
@@ -1,11 +1,18 @@
 import { Theme } from '@mui/material';
+import palette from './palette';
+function applyAlpha(color: string, alpha: number) {
+  const r = parseInt(color.slice(1, 3), 16);
+  const g = parseInt(color.slice(3, 5), 16);
+  const b = parseInt(color.slice(5, 7), 16);
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
 export const customShadows = [
   'none',
-  '0px 6px 10px 0px #0000000F',
-  '0px 10px 20px 0px #00000014',
-  '0px 14px 30px 0px #0000001A',
-  '0px 18px 40px 0px #0000001F',
-  '0px 22px 50px 0px #00000024',
-  '0px 26px 60px 0px #00000029',
+  `0px 6px 10px 0px ${applyAlpha(palette.black.main, 0.06)}`,
+  `0px 10px 20px 0px ${applyAlpha(palette.black.main, 0.08)}`,
+  `0px 14px 30px 0px ${applyAlpha(palette.black.main, 0.1)}`,
+  `0px 18px 40px 0px ${applyAlpha(palette.black.main, 0.12)}`,
+  `0px 22px 50px 0px ${applyAlpha(palette.black.main, 0.14)}`,
+  `0px 26px 60px 0px ${applyAlpha(palette.black.main, 0.16)}`,
   ...Array(18).fill('none'),
 ] as Theme['shadows'];
--- kit/src/lib/pages/login/index.tsx
+++ kit/src/lib/pages/login/index.tsx
@@ -155,23 +155,22 @@ export const Login = () => {
   const handleDropdownChange = (event: SelectChangeEvent<number | ''>) => {
     setDropdownValue(event.target.value as number | '');
   };
+  const StyledShadowBox = styled.div<{ shadow: string }>`
+    width: 120px;
+    height: 120px;
+    background-color: ${({ theme }) => theme.palette.white.main};
+    box-shadow: ${({ shadow, theme }) => theme.shadows[+shadow] || 'none'};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+    margin: 50px 0;
+  `;
 
   const ShadowBox = ({ shadow, label }: { shadow: string; label: string }) => (
-    <Box
-      sx={{
-        width: 120,
-        height: 120,
-        bgcolor: 'background.paper',
-        boxShadow: shadow,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: '20px',
-        margin: '50px 0',
-      }}
-    >
+    <StyledShadowBox shadow={shadow}>
       <Typography variant="caption">{label}</Typography>
-    </Box>
+    </StyledShadowBox>
   );
 
   return (
@@ -306,12 +305,12 @@ export const Login = () => {
       <Button loading={true} />
 
       <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 10 }}>
-        <ShadowBox shadow={1} label="Drop Shadow - 01" />
-        <ShadowBox shadow={2} label="Drop Shadow - 02" />
-        <ShadowBox shadow={3} label="Drop Shadow - 03" />
-        <ShadowBox shadow={4} label="Drop Shadow - 04" />
-        <ShadowBox shadow={5} label="Drop Shadow - 05" />
-        <ShadowBox shadow={6} label="Drop Shadow - 06" />
+        <ShadowBox shadow={'1'} label="Drop Shadow - 01" />
+        <ShadowBox shadow={'2'} label="Drop Shadow - 02" />
+        <ShadowBox shadow={'3'} label="Drop Shadow - 03" />
+        <ShadowBox shadow={'4'} label="Drop Shadow - 04" />
+        <ShadowBox shadow={'5'} label="Drop Shadow - 05" />
+        <ShadowBox shadow={'6'} label="Drop Shadow - 06" />
         <ShadowBox shadow="none" label="No Shadow" />
       </Box>
     </ThemeProvider>
